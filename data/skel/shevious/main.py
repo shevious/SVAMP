614713333c27ed5d7d5a0b56f39aaff549290e3d
shevious <shevious@yahoo.com>
2021-06-14 21:41:44 +0900

    skeleton

--- data/skel/shevious/main.py
+++ data/skel/shevious/main.py
@@ -1,1 +1,15 @@
+from src.prepare_infer import load_infer_data
 
+infer_ls = load_infer_data('/home/agc2021/dataset/problemsheet.json')
+#print(infer_ls)
+answers = {}
+for q in infer_ls:
+    answers[q['id']] = {
+        'answer': '0',
+        'equation': 'print(0)',
+    }
+
+import json
+json_path = 'answersheet.json'
+with open(json_path, 'w', encoding='UTF-8') as fout:
+    json.dump(answers, fout, ensure_ascii=False, indent=4)
