22d07cc350624003367adf41b062172ad0c21ec4
shevious <shevious@yahoo.com>
2021-06-15 19:54:38 +0900

    docker commands

--- data/data-chal/install.sh
+++ data/data-chal/install.sh
@@ -7,6 +7,19 @@
 #python3 -m pip install transformers==4.6.1
 #python3 -m pip install pandas==1.2.4 attrdict==2.0.1 nltk==3.4.5
 
+#ubuntu 18.04 cuda 10.2 python 3.8
 python3 -m pip install torch==1.8.1 torchtext==0.9.1 torchvision==0.9.1 transformers==4.6.1
+python3 -c "from transformers import AutoTokenizer; AutoTokenizer.from_pretrained('beomi/KcELECTRA-base')"
+python3 -c "from transformers import AutoModel; AutoModel.from_pretrained('beomi/KcELECTRA-base')"
+
+#ubuntu 18.04 cuda 11.1 python 3.8
+python3 -m pip install torch==1.9.0.dev20210526+cu111 torchtext==0.10.0.dev20210526 torchvision==0.10.0.dev20210526+cu111 -f "https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html"
+python3 -m pip install transformers==4.6.1
+python3 -c "from transformers import AutoTokenizer; AutoTokenizer.from_pretrained('beomi/KcELECTRA-base')"
+python3 -c "from transformers import AutoModel; AutoModel.from_pretrained('beomi/KcELECTRA-base')"
+
+#ubuntu 18.04 cuda 11.1 python 3.8
+python3 -m pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchtext==0.9.1 -f "https://download.pytorch.org/whl/torch_stable.html"
+python3 -m pip install transformers==4.6.1
 python3 -c "from transformers import AutoTokenizer; AutoTokenizer.from_pretrained('beomi/KcELECTRA-base')"
 python3 -c "from transformers import AutoModel; AutoModel.from_pretrained('beomi/KcELECTRA-base')"
